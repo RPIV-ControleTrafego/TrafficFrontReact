72a0fd685e0c956f9495e4d1b1568709ab89dc50
Mendes <andremendes0113@gmail.com>
2023-09-10 14:12:35 -0300

    Tailwind e consumo de api

--- trafficFront/src/App.jsx
+++ trafficFront/src/App.jsx
@@ -1,36 +1,66 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-class TrafficList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      trafficData: [],
-    };
-  }
+function App() {
+  const [data, setData] = useState([]);
 
-  componentDidMount() {
-    axios.get('/service/traffic/car-plate/list')
+  useEffect(() => {
+    axios.get('http://localhost:8082/service/traffic/car-plate/list')
       .then((response) => {
-        this.setState({ trafficData: response.data });
+        setData(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.log(error);
       });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <h2>Traffic Data</h2>
-        <ul>
-          {this.state.trafficData.map((data) => (
-            <li key={data.id}>{data.plateNumber} - {data.ownerName}</li>
-          ))}
-        </ul>
+  return (
+    <div className="App">
+      <h1 className="text-2xl font-bold mb-4 bg-blue ">Test</h1>
+      <div className="table-container">
+        <table className="table">
+          <thead>
+            <tr className='ml-8 bg-slate-600'>
+            <th>Car Plate</th>
+            <th>Car Type</th>
+            <th>Car Color</th>
+            <th>Car Brand</th>
+            <th>Vehicle Owner Name</th>
+            <th>Vehicle Owner CPF</th>
+            <th >Time</th>
+            <th>Date</th>
+            <th>Address</th>
+            <th>Speed</th>
+            <th>Max Speed</th>
+            <th>Direction</th>
+            <th>Street Direction</th>
+              {/* Adicione outras colunas do cabeçalho aqui */}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index} className="tr">
+               <td>{item.carPlate}</td>
+              <td>{item.carType}</td>
+              <td>{item.carColor}</td>
+              <td>{item.carBrand}</td>
+              <td>{item.veiculeOwnerName}</td>
+              <td>{item.veiculeOwneCPF}</td>
+              <td>{item.time}</td>
+              <td>{item.date}</td>
+              <td>{item.address}</td>
+              <td>{item.speed}</td>
+              <td>{item.maxSpeed}</td>
+              <td>{item.direction}</td>
+              <td>{item.streetDirection}</td>
+                {/* Adicione outras células de dados aqui */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default TrafficList;
+export default App;
