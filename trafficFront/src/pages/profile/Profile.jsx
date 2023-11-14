5b4c4f5b27ea795b059e557664b8f455b354d579
Mendes <andremendes0113@gmail.com>
2023-11-14 15:06:55 -0300

    Consulta de total de multas do usuario

--- trafficFront/src/pages/profile/Profile.jsx
+++ trafficFront/src/pages/profile/Profile.jsx
@@ -6,6 +6,9 @@ const Profile = () => {
   const [finesData, setFinesData] = useState([]);
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
+  const [selectedCurrency, setSelectedCurrency] = useState('real');
+  const [totalFinePrice, setTotalFinePrice] = useState(0);
+  const [latestInfraction, setLatestInfraction] = useState(null);
 
   useEffect(() => {
     const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
@@ -48,8 +51,37 @@ const Profile = () => {
     });
       };
 
-   
 
+  const fetchFineData = () => {
+  axios.get(`http://localhost:8086/infraction/total-fine-price/${selectedCurrency}/${user.cpf}`)
+    .then(response => {
+      setTotalFinePrice(response.data);
+    })
+    .catch(error => {
+      console.error('Erro ao obter o preço total da multa:', error);
+    });
+};
+
+
+const fetchLatestInfraction = () => {
+  axios.get(`http://localhost:8086/infraction/latest/${user.cpf}`)
+    .then(response => {
+      setLatestInfraction(response.data);
+    })
+    .catch(error => {
+      console.error('Erro ao obter a última multa:', error);
+    });
+};
+    
+      useEffect(() => {
+        fetchFineData();
+        console.log('Number of fines:', finesData.length);
+        console.log('Fines data:', finesData);
+      }, [finesData]);
+    
+      const handleCurrencyChange = () => {
+        fetchFineData();
+      };
 
 
 
@@ -63,7 +95,7 @@ const Profile = () => {
       <p><strong>Username:</strong> {user.username}</p>
       <p><strong>Email:</strong> {user.email}</p>
 
-      <div className="mt-6 mb-4">
+      {/* <div className="mt-6 mb-4">
         <label className="block text-sm font-medium text-gray-700">Data de Início:</label>
         <input
           type="date"
@@ -88,9 +120,10 @@ const Profile = () => {
         className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700"
       >
         Buscar Multas por Data
-      </button>
+      </button> */}
 
       <h2 className="text-2xl font-bold mt-6 mb-4">Dados de Multas</h2>
+      
       <ul className="list-none p-0">
         {finesData.map(fine => (
           <li key={fine.id} className="mb-2 flex justify-between items-center">
@@ -111,6 +144,40 @@ const Profile = () => {
           <p>Email: {user.email}</p>
           <p>Role: {user.role}</p>
           <p>CPF: {user.cpf}</p>
+      </div>
+
+      <div>
+          
+      <div className="mt-4 mb-4">
+      <div className="mt-4 mb-4">
+        <label className="block text-sm font-medium text-gray-700">Moeda:</label>
+        <select
+          value={selectedCurrency}
+          onChange={(e) => setSelectedCurrency(e.target.value)}
+          className="mt-1 p-2 border rounded-md"
+        >
+          <option value="real">Real</option>
+          <option value="dollar">Dollar</option>
+          <option value="euro">Euro</option>
+        </select>
+        <button
+          onClick={handleCurrencyChange}
+          className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700 ml-2"
+        >
+          Atualizar Moeda
+        </button>
+        
+        <p>Total a ser pago: {totalFinePrice}</p>
+        <button
+              onClick={() => payFine(fine.id)}
+              className="bg-green-500 text-white p-2 rounded-md hover:bg-green-700"
+            >
+              Pagar Multa
+            </button>
+        </div>
+        
+      </div>
+      
       </div>
     </div>
   );
