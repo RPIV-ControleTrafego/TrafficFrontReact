27e767f38932fb87a987273e516974d25a6d5763
ARTHUR9011 <72285627+ARTHUR9011@users.noreply.github.com>
2023-11-18 19:57:25 -0300

    fix: :bug: fix
    Correção do problema que não permitia a execução do programa

--- login/src/main/java/com/login/login/handler/ChainConfig.java
+++ login/src/main/java/com/login/login/handler/ChainConfig.java
@@ -2,9 +2,10 @@ package com.login.login.handler;
 
 public class ChainConfig {
     public static void configureChain() {
-        Handler authenticationHandler = new AuthenticationHandler();
-        Handler authorizationHandler = new AuthorizationHandler();
 
+        
+        AuthenticationHandler authenticationHandler = new AuthenticationHandler();
+        AuthorizationHandler authorizationHandler = new AuthorizationHandler(userRepository);
         authenticationHandler.setNext(authorizationHandler);
     }
 }
