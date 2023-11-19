ba6c2623522dc1253abc5e497d947513661d6fb2
ARTHUR9011 <72285627+ARTHUR9011@users.noreply.github.com>
2023-11-18 22:33:00 -0300

    fix: :bug: fix no repositorio
    O repositorio do usuarios estão causando problema ao executat

--- login/src/main/java/com/login/login/handler/ChainConfig.java
+++ login/src/main/java/com/login/login/handler/ChainConfig.java
@@ -1,8 +1,10 @@
+import com.login.login.repository.UserRepository; // Import the UserRepository class
 package com.login.login.handler;
+
 
 public class ChainConfig {
     public static void configureChain() {
-
+        UserRepository userRepository = new UserRepository();
         
         AuthenticationHandler authenticationHandler = new AuthenticationHandler();
         AuthorizationHandler authorizationHandler = new AuthorizationHandler(userRepository);
