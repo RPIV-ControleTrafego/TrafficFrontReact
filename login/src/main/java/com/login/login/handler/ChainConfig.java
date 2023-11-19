602786c18db494769b0063b4f3cb023ae93974e9
ARTHUR9011 <72285627+ARTHUR9011@users.noreply.github.com>
2023-11-18 23:24:23 -0300

    fix: :bug: erro config
    problema no AuthorizationHandler

--- login/src/main/java/com/login/login/handler/ChainConfig.java
+++ login/src/main/java/com/login/login/handler/ChainConfig.java
@@ -1,14 +1,31 @@
-import com.login.login.repository.UserRepository; // Import the UserRepository class
 package com.login.login.handler;
 
 
+import com.login.login.handler.AuthenticationHandler;
+
 public class ChainConfig {
     public static void configureChain() {
-        UserRepository userRepository = new UserRepository();
-        
+        UserRepository userRepository = UserRepository.getInstance(); 
         AuthenticationHandler authenticationHandler = new AuthenticationHandler();
         AuthorizationHandler authorizationHandler = new AuthorizationHandler(userRepository);
         authenticationHandler.setNext(authorizationHandler);
     }
+}
+
+class UserRepository {
+    private static UserRepository instance;
+
+    private UserRepository() {
+        // private constructor to prevent instantiation
+    }
+
+    public static UserRepository getInstance() {
+        if (instance == null) {
+            instance = new UserRepository();
+        }
+        return instance;
+    }
+
+    // other methods and fields...
 }
 
