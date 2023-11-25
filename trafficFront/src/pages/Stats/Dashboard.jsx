7c69b5898c19ad8b2ced9fe194097ed99e05e378
Júlio Silveira <julio.silveira@ayvu.net>
2023-11-25 10:02:09 -0300

    Update na página de Stats
    Adicionando separação entre dashboard e gráificos.

---
+++ trafficFront/src/pages/Stats/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+
+  function Dashboard() {
+    return (
+        <div className="App">
+          <div id="accordion-collapse" data-accordion="collapse" className='overflow-auto mb-96'>
+          <iframe
+            src="http://localhost:3000/public/dashboard/da6a70a3-5475-498e-90bc-8e97c4c27062"
+            frameborder="0"
+            width="1200"
+            height="800"
+            ></iframe>
+          </div>
+        </div>
+      );
+  } 
---
+++ trafficFront/src/pages/Stats/Graphics.jsx
@@ -0,0 +1,261 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { FileText } from 'react-feather';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [searchPlate, setSearchPlate] = useState('');
+  const [accordionExpanded, setAccordionExpanded] = useState(false);
+  const [accordionExpanded2, setAccordionExpanded2] = useState(false); 
+  const [accordionExpanded3, setAccordionExpanded3] = useState(false); 
+  const [accordionExpanded4, setAccordionExpanded4] = useState(false);
+  const [brandsData, setBrandsData] = useState([]);
+  const [carTypesData, setCarTypesData] = useState([]);
+
+  function fetchData() {
+    axios.get('http://localhost:8082/service/traffic/car-plate/list')
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function fetchBrandsData() {
+    axios.get('http://localhost:8082/service/traffic/car/brands')
+      .then((response) => {
+        setBrandsData(response.data); // Alterado para setBrandsData
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function fetchCarTypesData() {
+    axios.get('http://localhost:8082/service/traffic/car/types')
+      .then((response) => {
+        setCarTypesData(response.data); // Alterado para setCarTypesData
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }
+
+  useEffect(() => {
+    fetchCarTypesData();
+  }, []);
+
+  
+  
+  useEffect(() => {
+    fetchBrandsData();
+  }, []);
+
+  
+  useEffect(() => {
+    fetchData(); 
+  }, []);
+  
+
+  const toggleAccordion = () => {
+    setAccordionExpanded(!accordionExpanded);
+  };
+
+  const toggleAccordion2 = () => {
+    setAccordionExpanded2(!accordionExpanded2);
+  };
+
+  const toggleAccordion3 = () => {
+    setAccordionExpanded3(!accordionExpanded3);
+  };
+
+  const toggleAccordion4 = () => {
+    setAccordionExpanded4(!accordionExpanded4);
+  }
+
+  const downloadCSV = () => {
+    let csvContent = "carPlate,carType,carColor,carBrand,veiculeOwnerName,veiculeOwneCPF,time,date,address,speed,maxSpeed,direction,streetDirection\n";
+    infractionData.forEach((item) => {
+      const rowData = `${item.carPlate},${item.carType},${item.carColor},${item.carBrand},${item.veiculeOwnerName},${item.veiculeOwneCPF},${item.time},${item.date},${item.address},${item.speed},${item.maxSpeed},${item.direction},${item.streetDirection}\n`;
+      csvContent += rowData;
+    });
+  
+    const blob = new Blob([csvContent], { type: 'text/csv' });
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = 'data.csv';
+    link.click();
+  };
+  
+  return (
+    <div className="App">
+    <h1 className='text-5xl mb-12 mt-16'> Informações de Tráfego</h1>
+      <div id="accordion-collapse" data-accordion="collapse" className='overflow-auto mb-96'>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginTop:'100vh' }}>
+      </div>
+
+        <h2 id="accordion-collapse-heading-1">
+          <button
+            type="button"
+            className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+            data-accordion-target="#accordion-collapse-body-1"
+            aria-expanded={accordionExpanded}
+            aria-controls="accordion-collapse-body-1"
+            onClick={toggleAccordion}
+          >
+            <span>Lista de Dados Gerais</span>
+            <svg
+              data-accordion-icon
+              className={`w-3 h-3 rotate-${accordionExpanded ? '180' : '0'} shrink-0`}
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
+            </svg>
+          </button>
+        </h2>
+            <div
+            id="accordion-collapse-body-1"
+            className={accordionExpanded ? "p-5 border border-b-0 border-gray-200 dark:border-gray-700" : "hidden"}
+            aria-labelledby="accordion-collapse-heading-1"
+            >
+            <div>
+                <p className='flex'>
+                    Baixe os registros de tráfego em formato CSV.
+                </p>
+                <button onClick={downloadCSV} className='flex items-center mt-5
+                '>
+                Baixar CSV
+                <FileText size={20} style={{ marginLeft: '0.5em' }} />
+                </button>
+            </div>
+            </div>
+        <h2 id="accordion-collapse-heading-2">
+            <button
+                type="button"
+                className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+                data-accordion-target="#accordion-collapse-body-2"
+                aria-expanded={accordionExpanded2} 
+                aria-controls="accordion-collapse-body-2"
+                onClick={toggleAccordion2}
+            >
+                <span>Quantas marcas foram registrada?</span>
+                <svg
+                data-accordion-icon
+                className={`w-3 h-3 rotate-${accordionExpanded2 ? '180' : '0'} shrink-0`}
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+                >
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
+                </svg>
+            </button>
+        </h2>
+        <div
+        id="accordion-collapse-body-2"
+        className={accordionExpanded2 ? "p-5 border border-b-0 border-gray-200 dark:border-gray-700" : "hidden"}
+        aria-labelledby="accordion-collapse-heading-2"
+        >
+        <div className="flex justify-center items-center p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+
+
+        <iframe
+            style={{
+            background: '#FFFFFF',
+            border: 'none',
+            borderRadius: '2px',
+            boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
+            }}
+            width="1200"
+            height="630"
+            src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=65023917-a83c-4d99-8c0b-ffcd4285f4c6&maxDataAge=3600&theme=light&autoRefresh=true"
+        ></iframe>
+        </div>
+        </div>
+        
+
+
+        <h2 id="accordion-collapse-heading-3">
+        <button
+            type="button"
+            className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+            data-accordion-target="#accordion-collapse-body-3"
+            aria-expanded={accordionExpanded3} 
+            aria-controls="accordion-collapse-body-3"
+            onClick={toggleAccordion3}
+        >
+            <span>Quantas cores foram registradas?</span>
+            <svg
+            data-accordion-icon
+            className={`w-3 h-3 rotate-${accordionExpanded3 ? '180' : '0'} shrink-0`}
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+            >
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
+            </svg>
+        </button>
+        </h2>
+
+        <div
+        id="accordion-collapse-body-3"
+        className={accordionExpanded3 ? "p-5 border border-b-0 border-gray-200 dark:border-gray-700" : "hidden"}
+        aria-labelledby="accordion-collapse-heading-2"
+        >
+        <div className="flex justify-center items-center p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+            
+            <iframe
+            style={{
+                background: '#FFFFFF',
+                border: 'none',
+                borderRadius: '2px',
+                boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
+            }}
+            width="1200"
+            height="630"
+            src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=65023b00-9bed-4870-8a60-4722b5d657a9&maxDataAge=3600&theme=light&autoRefresh=true"
+            ></iframe>
+
+        </div>
+        </div>
+
+        <h2 id="accordion-collapse-heading-4">
+        <button
+            type="button"
+            className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+            data-accordion-target="#accordion-collapse-body-4"
+            aria-expanded={accordionExpanded4} 
+            aria-controls="accordion-collapse-body-4"
+            onClick={toggleAccordion4}
+        >
+            <span>Quantas cores foram registradas?</span>
+            <svg
+            data-accordion-icon
+            className={`w-3 h-3 rotate-${accordionExpanded4 ? '180' : '0'} shrink-0`}
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+            >
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
+            </svg>
+        </button>
+        </h2>
+        <div
+        id="accordion-collapse-body-4"
+        className={accordionExpanded4 ? "p-5 border border-b-0 border-gray-200 dark:border-gray-700" : "hidden"}
+        aria-labelledby="accordion-collapse-heading-2"
+        >
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
