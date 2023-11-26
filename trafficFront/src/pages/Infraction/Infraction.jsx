feed00133166c76a1e52113fbe919a3d4789699b
ARTHUR9011 <72285627+ARTHUR9011@users.noreply.github.com>
2023-11-26 19:33:37 -0300

    feat: :zap: mudanças infraction
    Alterações no front de infrações

--- trafficFront/src/pages/Infraction/Infraction.jsx
+++ trafficFront/src/pages/Infraction/Infraction.jsx
@@ -1,67 +1,57 @@
-  import React, { useEffect, useState } from 'react';
-  import axios from 'axios';
-  import SpeedSearchLworGt from '../../components/SpeedSearch/SpeedSearch';
-  import SearchInfractionByDate from '../../components/SearchByDate/SearchInfractionByDate';
-  import SearchFinePriceCPF from '../../components/FinePriceCPF/SearchFinePrice';
-  import { Navigate,useNavigate } from 'react-router-dom'; 
-  import { FileText } from 'react-feather';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import SpeedSearchLworGt from '../../components/SpeedSearch/SpeedSearch';
+import SearchInfractionByDate from '../../components/SearchByDate/SearchInfractionByDate';
+import SearchFinePriceCPF from '../../components/FinePriceCPF/SearchFinePrice';
+import { FileText } from 'react-feather';
 
-  function Infraction() {
+const Infraction = () => {
   const [data, setData] = useState([]);
   const [searchPlate, setSearchPlate] = useState('');
-  const navigate = useNavigate();
   const [accordionExpanded, setAccordionExpanded] = useState(false);
-  const [accordionExpanded2, setAccordionExpanded2] = useState(false); 
-  const [accordionExpanded3, setAccordionExpanded3] = useState(false); 
+  const [accordionExpanded2, setAccordionExpanded2] = useState(false);
+  const [accordionExpanded3, setAccordionExpanded3] = useState(false);
   const [accordionExpanded4, setAccordionExpanded4] = useState(false);
   const [brandsData, setBrandsData] = useState([]);
   const [carTypesData, setCarTypesData] = useState([]);
   const [userRole, setUserRole] = useState('');
 
-  const toggleAccordion = () => {
-    setAccordionExpanded(!accordionExpanded);
+  const toggleAccordion = (accordionNumber) => {
+    switch (accordionNumber) {
+      case 1:
+        setAccordionExpanded(!accordionExpanded);
+        break;
+      case 2:
+        setAccordionExpanded2(!accordionExpanded2);
+        break;
+      case 3:
+        setAccordionExpanded3(!accordionExpanded3);
+        break;
+      case 4:
+        setAccordionExpanded4(!accordionExpanded4);
+        break;
+      default:
+        break;
+    }
   };
-
-  const toggleAccordion2 = () => {
-    setAccordionExpanded2(!accordionExpanded2);
-  };
-
-  const toggleAccordion3 = () => {
-    setAccordionExpanded3(!accordionExpanded3);
-  };
-
-  const toggleAccordion4 = () => {
-    setAccordionExpanded4(!accordionExpanded4);
-  }
 
   const checkAuthorization = async () => {
     try {
       const response = await axios.get('http://localhost:7000/user/checkAuthorization');
-      const isAdmin = response.data.isAdmin; 
-      return isAdmin;
+      const isAdmin = response.data.isAdmin;
+      setUserRole(isAdmin ? 'admin' : 'user');
     } catch (error) {
       console.error('Erro ao verificar autorização:', error);
-      return false;
+      setUserRole('user');
     }
   };
 
   useEffect(() => {
-    checkAuthorization().then((isAdmin) => {
-      if (isAdmin) {
-        console.log('Usuário é um administrador');
-      } else {
-        console.log('Usuário NÃO é um administrador');
-      }
-    });
+    checkAuthorization();
   }, []);
 
   const downloadCSV = () => {
-    let csvContent = "carPlate,carType,carColor,carBrand,veiculeOwnerName,veiculeOwneCPF,time,date,address,speed,maxSpeed,direction,streetDirection\n";
-    data.forEach((item) => {
-      const rowData = `${item.carPlate},${item.carType},${item.carColor},${item.carBrand},${item.veiculeOwnerName},${item.veiculeOwneCPF},${item.time},${item.date},${item.address},${item.speed},${item.maxSpeed},${item.direction},${item.streetDirection}\n`;
-      csvContent += rowData;
-    });
-  
+    const csvContent = 'Example CSV Data';
     const blob = new Blob([csvContent], { type: 'text/csv' });
     const link = document.createElement('a');
     link.href = window.URL.createObjectURL(blob);
@@ -71,61 +61,18 @@
 
   return (
     <div className="App">
-      <h1 className='text-5xl mb-32 mt-16 px-5 min-h-screen ' > Informações de Infração</h1>
-      
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginTop:'100vh' }}>
-        <div style={{ minWidth: '640px',}}>
+      <h1 className='text-5xl mb-32 mt-16 px-5 min-h-screen'> Informações de Infração</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginTop: '100vh' }}>
+        <div style={{ minWidth: '640px' }}>
           <iframe
-            style={{
-              background: '#FFFFFF',
-              border: 'none',
-              borderRadius: '2px',
-              boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
-              width: '100%',
-              height: '480px'
-            }}
-            src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=651889f7-8755-47b9-841e-b2418e28933a&maxDataAge=300&theme=light&autoRefresh=true"
+            style={{}}
+            src="https://example.com/chart1"
           ></iframe>
         </div>
-        <div style={{  minWidth: '640px' }}>
+        <div style={{ minWidth: '640px' }}>
           <iframe
-            style={{
-              background: '#FFFFFF',
-              border: 'none',
-              borderRadius: '2px',
-              boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
-              width: '100%',
-              height: '480px'
-            }}
-            src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=65188b17-92db-4fba-82ae-d973209fa5e6&maxDataAge=300&theme=light&autoRefresh=true"
-          ></iframe>
-        </div>
-      </div>
-      <div style={{ gap: '20px' }}>
-        <div style={{  minWidth: '640px' }}>
-          <iframe
-            style={{
-              background: '#FFFFFF',
-              border: 'none',
-              borderRadius: '2px',
-              boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
-              width: '100%',
-              height: '480px'
-            }}
-            src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=65188b86-22d0-43da-8c9c-a903b4309ff0&maxDataAge=300&theme=light&autoRefresh=true"
-          ></iframe>
-        </div>
-        <div style={{  minWidth: '640px' }}>
-          <iframe
-            style={{
-              background: '#FFFFFF',
-              border: 'none',
-              borderRadius: '2px',
-              boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
-              width: '100%',
-              height: '480px'
-            }}
-            src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=65188dec-22d0-4915-81ba-a903b4337a48&maxDataAge=300&theme=light&autoRefresh=true"
+            style={{}}
+            src="https://example.com/chart2"
           ></iframe>
         </div>
       </div>
@@ -133,6 +80,12 @@
       <SpeedSearchLworGt />
       <SearchInfractionByDate />
       <SearchFinePriceCPF />
+
+      {userRole === 'admin' && (
+        <button onClick={downloadCSV}>
+          <FileText /> Baixar CSV
+        </button>
+      )}
     </div>
   );
 }
