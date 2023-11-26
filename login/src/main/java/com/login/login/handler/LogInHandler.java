385380abb5b66073524ac2b4e59dd0742e9215ec
ARTHUR9011 <72285627+ARTHUR9011@users.noreply.github.com>
2023-11-26 20:54:12 -0300

    feat: :sparkles: adição dos print casi acesso ao handler

--- login/src/main/java/com/login/login/handler/LogInHandler.java
+++ login/src/main/java/com/login/login/handler/LogInHandler.java
@@ -3,22 +3,42 @@ package com.login.login.handler;
 public abstract class LogInHandler {
     private LogInHandler successor;
 
+    public LogInHandler(LogInHandler successor) {
+        this.successor = successor;
+    }
+
     public void setSuccessor(LogInHandler successor) {
         this.successor = successor;
     }
 
     public boolean handleLogin(String username, String password) {
+        if (username == null || password == null) {
+            throw new IllegalArgumentException("Nome de usuário e senha não podem ser nulos.");
+        }
+
         if (authenticate(username, password)) {
-            System.out.println("Login bem-sucedido para o usuário: " + username);
+            printSuccessfulLogin(username);
             return true;
         } else if (successor != null) {
-            System.out.println("Tentativa de login falhou para o usuário: " + username);
+            printFailedAttempt(username);
             return successor.handleLogin(username, password);
         } else {
-            System.out.println("Usuário " + username + " não encontrado.");
+            userNotFound(username);
             return false;
         }
     }
 
     protected abstract boolean authenticate(String username, String password);
+
+    private void printSuccessfulLogin(String username) {
+        System.out.println("Login bem-sucedido para o usuário: " + username);
+    }
+
+    private void printFailedAttempt(String username) {
+        System.out.println("Tentativa de login falhou para o usuário: " + username);
+    }
+
+    private void userNotFound(String username) {
+        System.out.println("Usuário " + username + " não encontrado.");
+    }
 }
