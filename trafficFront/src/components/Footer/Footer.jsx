2d163a9bc7c75e9f07180f936ec025e9aeabfb7a
Júlio Silveira <julio.silveira@ayvu.net>
2023-12-18 21:09:42 -0300

    Front

--- trafficFront/src/components/Footer/Footer.jsx
+++ trafficFront/src/components/Footer/Footer.jsx
@@ -68,8 +68,8 @@ const Footer = () => {
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023 <a href="https://flowbite.com/" className="hover:underline">Traffic Today System™</a>. All Rights Reserved.
+        <span className="block text-sm text-white sm:text-center dark:text-gray-400">
+  © 2023 <a href="https://flowbite.com/" className="hover:underline text-white">Traffic Today System™</a>. All Rights Reserved.
         </span>
       </div>
     </footer>
--- trafficFront/src/components/Navbar/Navbar.jsx
+++ trafficFront/src/components/Navbar/Navbar.jsx
@@ -110,7 +110,11 @@ const Navbar = ({ loggedInUser }) => {
 
             {isPolice && !isAdmin && (
               <li>
-                <NavLink to="/infraction">Infrações</NavLink>
+                <NavLink to="/infraction"
+                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                activeClassName="bg-blue-700"
+                exact
+                >Infrações</NavLink>
               </li>
             )}
 
