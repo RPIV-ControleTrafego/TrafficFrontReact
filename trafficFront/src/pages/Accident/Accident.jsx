099735d3444b1893f0e1224f8362d78fef32a0b6
Mendes <andremendes0113@gmail.com>
2023-12-18 22:23:28 -0300

    Gráficos de acidentes

--- trafficFront/src/pages/Accident/Accident.jsx
+++ trafficFront/src/pages/Accident/Accident.jsx
@@ -17,12 +17,12 @@ import axios from 'axios';
   const [data, setData] = useState([]);
   const [searchPlate, setSearchPlate] = useState('');
   const [accordionExpanded, setAccordionExpanded] = useState(false);
-  const [accordionExpanded2, setAccordionExpanded2] = useState(false); 
-  const [accordionExpanded3, setAccordionExpanded3] = useState(false); 
+  const [accordionExpanded2, setAccordionExpanded2] = useState(false);
+  const [accordionExpanded3, setAccordionExpanded3] = useState(false);
   const [accordionExpanded4, setAccordionExpanded4] = useState(false);
   const [brandsData, setBrandsData] = useState([]);
   const [carTypesData, setCarTypesData] = useState([]);
- 
+
   const toggleAccordion = () => {
     setAccordionExpanded(!accordionExpanded);
   };
@@ -39,13 +39,48 @@ import axios from 'axios';
     setAccordionExpanded4(!accordionExpanded4);
   }
 
+
+
+{/* <iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=6580ebd1-6716-4ad9-8fdf-ef9da635ec28&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
+
+
+<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=6580ec3c-613e-4c7b-8c19-8bf495096059&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
+
+
+<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=6580ec86-a8d1-47d1-8659-08eeef1fa176&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
+
+
+<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=6580ece2-613e-425f-8c8b-8bf49520b282&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
+
+
+<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=6580ed52-163d-4e1e-8cf9-21e63a7a7230&maxDataAge=3600&theme=light&autoRefresh=true"></iframe> */}
+
+  const charts = [
+    'https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=6580ebd1-6716-4ad9-8fdf-ef9da635ec28&maxDataAge=3600&theme=light&autoRefresh=true',
+    'https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=6580ec3c-613e-4c7b-8c19-8bf495096059&maxDataAge=3600&theme=light&autoRefresh=true',
+    'https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=6580ec86-a8d1-47d1-8659-08eeef1fa176&maxDataAge=3600&theme=light&autoRefresh=true',
+    'https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=6580ece2-613e-425f-8c8b-8bf49520b282&maxDataAge=3600&theme=light&autoRefresh=true',
+    'https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=6580ed52-163d-4e1e-8cf9-21e63a7a7230&maxDataAge=3600&theme=light&autoRefresh=true',
+
+
+  ]
+
+  const chartStyle = {
+    background: '#FFFFFF',
+    border: 'none',
+    borderRadius: '2px',
+    boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
+    width: '100%',
+    height: '480px',
+  };
+
   const downloadCSV = () => {
     let csvContent = "date,hasInjuries,hasFatalities,address,hasInfraction\n";
     data.forEach((item) => {
       const rowData = `${item.data},${item.hasInjuries},${item.hasFatalities},${item.address},${item.hasInfraction}\n`;
       csvContent += rowData;
     });
-  
+
     const blob = new Blob([csvContent], { type: 'text/csv' });
     const link = document.createElement('a');
     link.href = window.URL.createObjectURL(blob);
@@ -54,15 +89,15 @@ import axios from 'axios';
   };
   return (
     <div className="App">
-
+{/*
     <iframe
-        src={acidentes}
+        src={}
         frameBorder={0}
         width={800}
         height={600}
         allowTransparency
-    />
-      
+    /> */}
+
     <h1 className='text-5xl mb-12 mt-16'> Informações de Acidentes</h1>
       <div id="accordion-collapse" data-accordion="collapse" className='overflow-auto mb-96'>
         <h2 id="accordion-collapse-heading-1">
@@ -108,7 +143,7 @@ import axios from 'axios';
     type="button"
     className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
     data-accordion-target="#accordion-collapse-body-2"
-    aria-expanded={accordionExpanded2} 
+    aria-expanded={accordionExpanded2}
     aria-controls="accordion-collapse-body-2"
     onClick={toggleAccordion2}
   >
@@ -125,112 +160,16 @@ import axios from 'axios';
     </svg>
   </button>
 </h2>
-<div
-  id="accordion-collapse-body-2"
-  className={accordionExpanded2 ? "p-5 border border-b-0 border-gray-200 dark:border-gray-700" : "hidden"}
-  aria-labelledby="accordion-collapse-heading-2"
->
-<div className="flex justify-center items-center p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-  <iframe
-    style={{
-      background: '#FFFFFF',
-      border: 'none',
-      borderRadius: '2px',
-      boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
-    }}
-    width="1200"
-    height="630"
-    src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=65023917-a83c-4d99-8c0b-ffcd4285f4c6&maxDataAge=3600&theme=light&autoRefresh=true"
-  ></iframe>
-  </div>
-</div>
-
-<h2 id="accordion-collapse-heading-3">
-  <button
-    type="button"
-    className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-    data-accordion-target="#accordion-collapse-body-3"
-    aria-expanded={accordionExpanded3} 
-    aria-controls="accordion-collapse-body-3"
-    onClick={toggleAccordion3}
-  >
-    <span>Quantas cores foram registradas?</span>
-    <svg
-      data-accordion-icon
-      className={`w-3 h-3 rotate-${accordionExpanded3 ? '180' : '0'} shrink-0`}
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 10 6"
-    >
-      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
-    </svg>
-  </button>
-</h2>
-<div
-  id="accordion-collapse-body-3"
-  className={accordionExpanded3 ? "p-5 border border-b-0 border-gray-200 dark:border-gray-700" : "hidden"}
-  aria-labelledby="accordion-collapse-heading-2"
->
-<div className="flex justify-center items-center p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-    
-      <iframe
-      style={{
-        background: '#FFFFFF',
-        border: 'none',
-        borderRadius: '2px',
-        boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
-      }}
-      width="1200"
-      height="630"
-      src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=65023b00-9bed-4870-8a60-4722b5d657a9&maxDataAge=3600&theme=light&autoRefresh=true"
-    ></iframe>
-
-  </div>
-</div>
-
-<h2 id="accordion-collapse-heading-4">
-  <button
-    type="button"
-    className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-    data-accordion-target="#accordion-collapse-body-4"
-    aria-expanded={accordionExpanded4} 
-    aria-controls="accordion-collapse-body-4"
-    onClick={toggleAccordion4}
-  >
-    <span>Quantas cores foram registradas?</span>
-    <svg
-      data-accordion-icon
-      className={`w-3 h-3 rotate-${accordionExpanded4 ? '180' : '0'} shrink-0`}
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 10 6"
-    >
-      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
-    </svg>
-  </button>
-</h2>
-<div
-  id="accordion-collapse-body-4"
-  className={accordionExpanded4 ? "p-5 border border-b-0 border-gray-200 dark:border-gray-700" : "hidden"}
-  aria-labelledby="accordion-collapse-heading-2"
->
-<div className="flex justify-center items-center p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-      <iframe
-      style={{
-        background: '#FFFFFF',
-        border: 'none',
-        borderRadius: '2px',
-        boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
-      }}
-      width="1200"
-      height="630"
-      src="https://charts.mongodb.com/charts-project-0-yqqda/embed/charts?id=65023b20-c954-44bf-89be-5daa9d31be66&maxDataAge=300&theme=light&autoRefresh=true"
-    ></iframe>
-          </div>
+<div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '20px', marginTop: 600 }}>
+          {charts.map((chart, index) => (
+            <div key={index} style={{ minWidth: '640px' }}>
+              <iframe style={chartStyle} src={chart}></iframe>
+            </div>
+          ))}
         </div>
-      </div>
+    </div>
+
+
     </div>
   );
 }
